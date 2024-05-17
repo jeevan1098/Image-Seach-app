c411c924c7dd27b89104ff57ee5cb708c436ab2f
G Jeevan Babu <jeevanbabugotru29@gmail.com>
2024-05-17 08:51:15 +0000

    added image grid

--- image_search_app/src/App.js
+++ image_search_app/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import ImageGrid from "./components/ImageGrid";
 import "./App.css";
 
 const App = () => {
   return (
     <div className="App">
       <Header />
-      <p>Welcome to Jeevan Image Search</p>
     </div>
   );
 };
